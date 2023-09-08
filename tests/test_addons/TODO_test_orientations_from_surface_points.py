7a8e7d0d1964b8da53e56dce5423d7f698207c34
MigueldelaVarga <miguel@terranigma-solutions.com>
2023-09-08 11:43:34 +0200

    - [ENH] Added the option for orientations from surface points

--- tests/test_addons/TODO_test_orientations_from_surface_points.py
+++ tests/test_addons/TODO_test_orientations_from_surface_points.py
@@ -1,25 +1,34 @@
 import gempy as gp
 import numpy as np
 import os
+
 input_path = os.path.dirname(__file__) + '/../../examples/data'
+
+data_path = os.path.abspath('../data')
 
 
 def test_set_orientations():
     # Importing the data from CSV-files and setting extent and resolution
-    geo_data = gp.create_data_legacy(extent=[0, 2000, 0, 2000, 0, 2000], resolution=[50, 50, 50],
-                                     path_o=input_path + '/input_data/tut_chapter1/simple_fault_model_orientations.csv',
-                                     path_i=input_path + '/input_data/tut_chapter1/simple_fault_model_points.csv')
+    geo_data = gp.create_geomodel(
+        extent=[0, 2000, 0, 2000, 0, 2000],
+        resolution=[50, 50, 50],
+        importer_helper=gp.data.ImporterHelper(
+            path_to_orientations=f"{data_path}/model5_orientations.csv",
+            path_to_surface_points=f"{data_path}/model5_surface_points.csv",
+        )
+    )
 
-    gp.get_data(geo_data)
+    orientations: gp.data.OrientationsTable = gp.create_orientations_from_surface_points(geo_data.surface_points)
 
-    # Assigning series to formations as well as their order (timewise)
-    gp.map_stack_to_surfaces(geo_data, {"Fault_Series": 'Main_Fault',
-                                         "Strat_Series": ('Sandstone_2', 'Siltstone')})
+    gp.add_orientations(
+        geo_model=geo_data,
+        x=orientations.data['X'],
+        y=orientations.data['Y'],
+        z=orientations.data['Z'],
+        pole_vector=orientations.grads,
+        elements_names=geo_data.structural_frame.elements_names[0],
+    )
 
-    geo_data._orientations.create_orientation_from_surface_points(geo_data.surface_points, [0, 1, 2])
-
-    gp.set_orientation_from_surface_points(geo_data, [0, 1, 2])
-    
 
 def test_select_nearest_surface_points():
     data_path = 'https://raw.githubusercontent.com/cgre-aachen/gempy_data/master/'
@@ -51,7 +60,7 @@ def test_select_nearest_surface_points():
     radius = [r.sort_values() for r in radius]
 
     # define reference
-    reference = [[16,17],[16,17],[18,19],[18,19],[20,21],[20,21]]
+    reference = [[16, 17], [16, 17], [18, 19], [18, 19], [20, 21], [20, 21]]
 
     assert np.array_equal(reference, knn) and np.array_equal(reference, radius)
 
@@ -67,7 +76,7 @@ def test_set_orientation_from_neighbours():
 
     # Assigning series to formations as well as their order (timewise)
     gp.map_stack_to_surfaces(geo_data, {"Fault_Series": 'fault',
-                                    "Strat_Series": ('rock2', 'rock1')})
+                                        "Strat_Series": ('rock2', 'rock1')})
     geo_data.set_is_fault(['Fault_Series'])
 
     # detect fault names
@@ -84,7 +93,7 @@ def test_set_orientation_from_neighbours():
     test = geo_data._orientations.df.sort_index().iloc[-1][['dip', 'azimuth']].values
 
     # calculate reference
-    reference = [90-np.arctan(0.5)/np.pi*180, 90]
+    reference = [90 - np.arctan(0.5) / np.pi * 180, 90]
 
     assert np.array_equal(reference, test)
 
@@ -110,4 +119,4 @@ def test_set_orientation_from_neighbours_all():
     length_after = geo_data._orientations.df.shape[0]
 
     assert np.array_equal(geo_data._surface_points.df.shape[0],
-                          length_after-length_pre)
+                          length_after - length_pre)
